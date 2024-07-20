d18eb6dfd2a0c0e276515c63028afd35736993bd
AhmetMuratAcar <neistersenat@gmail.com>
2024-07-20 04:57:07 -0700

    add HTMLNode

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        stringified = ""
+        for key, value in self.props.items():
+            stringified += f' {key}="{value}"'
+
+        return stringified
+
+    def __repr__(self):
+        return f"tag = {self.tag}\nvalue = {self.value}\nchildren = {self.children}\nprops = {self.props}"
